a350d67e4d320c168b5e85b964be81a3cc66af60
Egraz Marlene <marlene.egraz@gmail.com>
2017-05-18 09:10:52 +0200

    changements background images etc

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-//mes perso
-let perso = {
-    nom: "Han Solo",
+let stats = ['hp', 'mp', 'attack'];
+for (let x = 0; x < stats.length; x++) {
 
 }
+
+
+
+
+//mes personnages
+let perso1 = {
+    nom: "Vader",
+    pointsvie: 100,
+    energie: 380
+}
+let perso2 = {
+    nom: "Yoda",
+    pointsvie: 100,
+    energie: 300
+}
+
+//attaques
+
+function()
